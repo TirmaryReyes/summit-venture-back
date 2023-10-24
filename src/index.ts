70cd7cfdb75572587148e9c5140ccdf3fa0f241c
Tirmary <tmrv1991@gmail.com>
2023-10-24 16:26:31 +0300

    Add Express server startup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-const a = 3;
+import chalk from "chalk";
+import app from "./server/app";
+import createDebug from "debug";
+
+const port = process.env.PORT ?? 4000;
+
+const debug = createDebug("societ-api:root");
+
+app.listen(port, () => {
+  debug(chalk.blueBright(`Listening in port ${port}`));
+});
