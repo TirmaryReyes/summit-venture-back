ae1cd1d61711c5f10017b7799edd1b905e5dafa7
Tirmary <tmrv1991@gmail.com>
2023-10-30 14:49:56 +0300

    Create generalError error

--- src/server/middlewares/errorMiddlewares/errorMiddlewares.ts
+++ src/server/middlewares/errorMiddlewares/errorMiddlewares.ts
@@ -10,8 +10,22 @@ export const notFoundError = (
   _res: Response,
   next: NextFunction,
 ) => {
-  debug(chalk.red("not found Error"));
   const error = new CustomError("Endpoint not found", 400);
 
   next(error);
+};
+
+export const generalError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  debug(`Error: ${chalk.red(error.message)}`);
+
+  const statusCode = error.statusCode || 500;
+
+  const message = error.statusCode ? error.message : "General error";
+
+  res.status(statusCode).json({ message });
 };
